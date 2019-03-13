0a9c349af3ee5c0173afed7dd7025f38608938a4
geoffOkumu <geoffocum@gmail.com>
2019-03-13 10:53:44 +0300

    search functionality

--- src/components/containers/Search.jsx
+++ src/components/containers/Search.jsx
@@ -1,8 +1,98 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+import uniq from 'lodash/uniq'
+import kebabCase from 'lodash/kebabCase'
+import { Link } from 'gatsby'
+import Fuse from 'fuse.js'
 
 import Input from '../widgets/Input'
 import Container from '../widgets/Container'
+import Heading from '../widgets/Heading'
+import Text from '../widgets/Text'
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchtext: '',
+      searchReasults: [],
+    }
+
+    //Init fuse.js
+    let options = {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 10,
+      keys: ['title', 'category'],
+    }
+
+    let list = this.props.posts.map(post => post.node.frontmatter)
+
+    this.fuseSearch = new Fuse(list, options)
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    let { searchtext } = this.state
+
+    let results = this.fuseSearch.search(searchtext)
+
+    this.setState({ searchReasults: results })
+  }
+
+  handleInputChange = e => {
+    this.setState({ searchtext: e.target.value })
+
+    let { searchtext } = this.state
+
+    let results = this.fuseSearch.search(searchtext)
+
+    this.setState({ searchReasults: results })
+  }
+
+  render() {
+    const { searchtext, searchReasults } = this.state
+    const { posts } = this.props
+
+    const categories = uniq(posts.map(post => post.node.frontmatter.category))
+
+    return (
+      <Container customStyles={customStyles.container}>
+        <Form>
+          <Input
+            value={searchtext}
+            onChange={this.handleInputChange}
+            customStyles={customStyles.input}
+            placeholder="Search..."
+          />
+        </Form>
+        <section>
+          {searchReasults.map(result => (
+            <SearchReasultItem key={result.title}>
+              <Link to={`/${kebabCase(result.title)}`}>
+                <Heading.h2>{result.title}</Heading.h2>
+                <Text.span>{result.category}</Text.span>
+              </Link>
+            </SearchReasultItem>
+          ))}
+        </section>
+        <CategoriesWrapper>
+          <Heading.h3>Categories</Heading.h3>
+          {categories.map(category => (
+            <Link to={`/categories/${kebabCase(category)}`} key={category}>
+              {category}
+            </Link>
+          ))}
+        </CategoriesWrapper>
+      </Container>
+    )
+  }
+}
 
 const customStyles = {
   container: css`
@@ -20,16 +110,45 @@ const Form = styled.form`
   display: block;
 `
 
-class Search extends React.Component {
-  render() {
-    return (
-      <Container customStyles={customStyles.container}>
-        <Form>
-          <Input customStyles={customStyles.input} placeholder="Search..." />
-        </Form>
-      </Container>
-    )
+const CategoriesWrapper = styled.section`
+  padding-top: 2rem;
+  text-align: center;
+  text-transform: capitalize !important;
+
+  a {
+    padding: 0 1rem;
+
+    &::before {
+      content: '# ';
+    }
   }
-}
+`
+
+const SearchReasultItem = styled.div`
+  display: block;
+  width: 100%;
+  padding-top: 0.5rem;
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.textDark};
+
+    &:hover h2 {
+      text-decoration: underline;
+    }
+  }
+
+  h2 {
+    margin-bottom: 4px;
+  }
+
+  span {
+    text-transform: capitalize;
+
+    &::before {
+      content: '# ';
+    }
+  }
+`
 
 export default Search
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,16 +1,49 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Search from '../components/containers/Search'
 import Layout from '../components/containers/Layout'
 import Header from '../components/containers/Header'
 
-const SearchPage = ({ location }) => {
+const SearchPage = ({ location, data }) => {
+  const posts = data.posts.edges
   return (
     <Layout>
-      <Header location={location} />
-      <Search />
+      <Wrapper>
+        <div style={{ height: 80 }} />
+        <Header location={location} />
+        <Search posts={posts} />
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.bodyBg};
+  min-height: 80vh;
+  padding-bottom: 4rem;
+`
+
+export const categoriesQuery = graphql`
+  query CategoriesQuery {
+    posts: allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "blog" } } }
+      limit: 1000
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            category
+          }
+        }
+      }
+    }
+  }
+`
 
 export default SearchPage
